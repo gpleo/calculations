fbebd22cbb877cee1234c1e7d89e894b2c870567
Leo <leo@u3dspace.com>
2020-06-04 09:49:30 +0800

    add calculations

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      questions: [],
+    }
+  }
+
+  componentDidMount() {
+    const questions = [];
+    for (let i = 0; i < 100;) {
+      let s1 = Math.floor(Math.random() * (100));
+      let s2 = Math.floor(Math.random() * (100));
+      const s3 = Math.floor(Math.random() * 2);
+      let answer;
+      if (s3 === 0 && s1 + s2 > 100) {
+        continue;
+      }
+
+      if (s3 === 1 && s1 < s2) {
+        const s = s1;
+        s1 = s2;
+        s2 = s;
+      }
+
+      if (s3 === 0) {
+        answer = s1 + s2;
+      } else {
+        answer = s1 - s2;
+      }
+
+      questions.push([s1, s2, s3, answer]);
+      i++;
+    }
+    this.setState({
+      questions,
+    })
+  }
+
+  print = () => {
+    const currentInnerHTML = window.document.body.innerHTML;
+    window.document.body.innerHTML = window.document.getElementById('calculations').innerHTML;
+    window.print();
+    window.document.body.innerHTML = currentInnerHTML;
+  };
+
+  render() {
+    return (
+      <div style={{ margin: 10 }}>
+        <Button type="primary" onClick={this.print} style={{ marginBottom: 10 }}>打印</Button>
+        <div id="calculations">
+          {
+            this.state.questions.map((v, i) => {
+              const q = <span key={i} style={{ display: 'inline-block', width: '25%' }}>{`${v[0]} ${v[2] === 0 ? '+' : '-'} ${v[1]} = ${v[3]}`}</span>;
+              if (i % 4 === 0) {
+                return [<br key={`br${i}`}/>, q];
+              }
+              return q;
+            })
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
