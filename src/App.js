f9a7fe5a4773ceb12d3052f67c2ac0939809c813
Leo <leo@u3dspace.com>
2020-06-09 08:22:15 +0800

    增加连续加减的支持

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Button, Radio, Switch } from 'antd';
+import { Button, Radio, Switch, Checkbox } from 'antd';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      questionCount: 180,
+      questionCount: 160,
       questions: [],
       controlsVisible: true,
       showAnswer: false,
+
+      // 两数加减
+      normal: true,
+      // 连续加减
+      continuous: false,
     }
   }
 
@@ -17,30 +22,18 @@ class App extends React.Component {
     this.generateQuestions(this.state.questionCount);
   }
 
-  generateQuestions = (count) => {
+  generateQuestions = () => {
     const questions = [];
-    for (let i = 0; i < count;) {
-      let s1 = Math.floor(Math.random() * (100));
-      let s2 = Math.floor(Math.random() * (100));
-      const s3 = Math.floor(Math.random() * 2);
-      let answer;
-      if (s3 === 0 && s1 + s2 > 100) {
-        continue;
+    for (let i = 0; i < this.state.questionCount;) {
+      let number = Math.floor(Math.random() * (100));
+      let result = generate([number], [], number);
+      if (this.state.continuous) {
+        if (!this.state.normal || Math.floor(Math.random() * 2) === 0) {
+          result = generate(result.numbers, result.operations, result.answer);
+        }
       }
 
-      if (s3 === 1 && s1 < s2) {
-        const s = s1;
-        s1 = s2;
-        s2 = s;
-      }
-
-      if (s3 === 0) {
-        answer = s1 + s2;
-      } else {
-        answer = s1 - s2;
-      }
-
-      questions.push([s1, s2, s3, answer]);
+      questions.push(result);
       i++;
     }
     this.setState({
@@ -59,22 +52,35 @@ class App extends React.Component {
     });
   };
 
-  refresh = () => {
-    this.generateQuestions(this.state.questionCount);
-  };
-
   handleQuestionCountChange = (e) => {
     const count = e.target.value;
     this.setState({
       questionCount: count,
+    }, () => {
+      this.generateQuestions();
     });
-    this.generateQuestions(count);
   };
 
   handleAnswerSwitch = v => {
     this.setState({
       showAnswer: v,
     })
+  };
+
+  handleNormalChange = e => {
+    this.setState({
+      normal: e.target.checked,
+    }, () => {
+      this.generateQuestions();
+    });
+  };
+
+  handleContinuousChange = e => {
+    this.setState({
+      continuous: e.target.checked,
+    }, () => {
+      this.generateQuestions();
+    });
   };
 
   render() {
@@ -84,10 +90,14 @@ class App extends React.Component {
           this.state.controlsVisible && (
             <div style={{ marginBottom: 10 }}>
               <Radio.Group onChange={this.handleQuestionCountChange} style={{ marginRight: 10 }} value={this.state.questionCount}>
-                <Radio.Button value={90}>90题</Radio.Button>
-                <Radio.Button value={180}>180题</Radio.Button>
+                <Radio.Button value={80}>80题</Radio.Button>
+                <Radio.Button value={160}>160题</Radio.Button>
               </Radio.Group>
-              <Button type="primary" onClick={this.refresh} style={{ marginRight: 10 }}>重新生成</Button>
+
+              <Checkbox checked={this.state.normal} onClick={this.handleNormalChange}>两数加减</Checkbox>
+              <Checkbox checked={this.state.continuous} onClick={this.handleContinuousChange}>连续加减</Checkbox>
+
+              <Button type="primary" onClick={this.generateQuestions} style={{ marginRight: 10 }}>重新生成</Button>
               <Button type="primary" onClick={this.print} style={{ marginRight: 10 }}>打印</Button>
               <Switch
                 checked={this.state.showAnswer}
@@ -118,15 +128,15 @@ class App extends React.Component {
           {
             this.state.questions.map((v, i) => {
               const answer = this.state.showAnswer
-                ? <span style={{ color: 'red' }}>{v[3]}</span>
+                ? <span style={{ color: 'red' }}>{v.answer}</span>
                 : '';
               const q = (
                 <span
                   key={i}
-                  style={{ display: 'inline-block', width: '25%' }}
+                  style={{ display: 'inline-block', width: '25%', fontSize: 16 }}
                 >
                   {
-                    `${v[0]} ${v[2] === 0 ? '+' : '-'} ${v[1]} = `
+                    print(v)
                   }
                   {answer}
                 </span>
@@ -142,5 +152,58 @@ class App extends React.Component {
     );
   }
 }
+
+const generate = (numbers, operations, answer) => {
+  const result = generateCore(answer);
+
+  numbers = [...numbers, result.number];
+  operations = [...operations, result.operation];
+  answer = result.answer;
+
+  return {
+    numbers, operations, answer
+  }
+};
+
+const generateCore = num1  => {
+  let num2, operation;
+  while(true) {
+    num2 = Math.floor(Math.random() * (100));
+    operation = Math.floor(Math.random() * 2) === 0 ? '+' : '-';
+    if (operation === '+' && num1 + num2 > 100) {
+      continue;
+    }
+
+    if (operation === '-' && num1 < num2) {
+      operation = '+';
+      if (num1 + num2 > 100) {
+        continue;
+      }
+    }
+    break;
+  }
+
+  let answer;
+  if (operation === '+') {
+    answer = num1 + num2;
+  } else {
+    answer = num1 - num2;
+  }
+
+  return {
+    number: num2,
+    operation,
+    answer,
+  };
+};
+
+const print = v => {
+  let result = v.numbers[0];
+  for (let i = 0; i < v.operations.length; i++) {
+    result += ` ${v.operations[i]} ${v.numbers[i+1]}`;
+  }
+  result += ' = ';
+  return result;
+};
 
 export default App;
